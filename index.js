bb2cce11cf847e0c00d7adee797be1ac41029662
aditya-koul <161067848+aditya-koul@users.noreply.github.com>
2024-11-27 16:50:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -55,12 +55,4 @@ const docs = {
 
 app.post('/functions/base64Encode', base64Encode);
 
-const PORT = process.env.PORT || 3000;
-
-if(process.env.NODE_ENV !== 'test') {
-    app.listen('PORT', () => {
-        console.log('Server running on port ' + PORT)
-    });
-}
-
 export default app;
