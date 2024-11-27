66ac1c60a477d74be63d185a09bbaceb001141d7
aditya-koul <161067848+aditya-koul@users.noreply.github.com>
2024-11-27 16:47:34 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,66 @@
+import express from 'express';
+import bodyParser from 'body-parser'
+
+const app = express();
+
+app.use(express.json());
+app.use(bodyParser.json());
+
+export const base64Encode = async (req, res) => {
+    try {
+      
+      const { input } = req.body;
+  
+      if (!input) {
+        return res.status(400).json({ 
+          error: 'Input is required',
+          output: null 
+        });
+      }
+  
+    
+      const output = Buffer.from(input).toString('base64');
+  
+      // Send response
+      res.json({ output });
+    } catch (error) {
+      // Handle any unexpected errors
+      res.status(500).json({ 
+        error: 'An unexpected error occurred',
+        output: null 
+      });
+    }
+  };
+
+
+
+const docs = {
+    name: "base64Encode",
+    description: "Encode anything to base64",
+    input: {
+      type: "string",
+      description: "Input the data you'd like to encode to base64",
+      example: "Hello, world"
+    },
+    output: {
+      type: "string",
+      description: "Base64 encoded string",
+      example: "SGVsbG8sIHdvcmxk"
+    }
+  };
+
+  app.get('/functions/base64Encode', (req, res) => {
+    res.json(docs);
+  });
+
+app.post('/functions/base64Encode', base64Encode);
+
+const PORT = process.env.PORT || 3000;
+
+if(process.env.NODE_ENV !== 'test') {
+    app.listen('PORT', () => {
+        console.log('Server running on port ' + PORT)
+    });
+}
+
+export default app;
